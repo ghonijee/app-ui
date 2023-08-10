62c2b8b073ead0dca3a122e973b572e6ba4f914e
ghonijee <akughoni@gmail.com>
2023-08-10 16:02:55 +0700

    chore: change color onBackground to skyLighter

--- lib/theme/app_color_extension.dart
+++ lib/theme/app_color_extension.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 
 class AppColorExtension extends ThemeExtension<AppColorExtension> {
   final Color background = FigmaColors.inkDarkest;
-  final Color onBackground = FigmaColors.skyBase;
+  final Color onBackground = FigmaColors.skyLighter;
   final Color surface = FigmaColors.inkDarker;
   final Color onSurface = FigmaColors.skyWhite;
   final primary = _Primary();
